82e763dfa56749c3ddbb81e91b564f75f7fa3c96
KewenC <KewenC@outlook.com>
2018-11-29 03:05:24 +0800

    更新

--- app/src/main/java/com/kewenc/radialgradient/MainActivity.java
+++ app/src/main/java/com/kewenc/radialgradient/MainActivity.java
@@ -1,11 +1,13 @@
 package com.kewenc.radialgradient;
 
+import android.graphics.Color;
 import android.graphics.Outline;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -20,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
 //        SweepGradientView sweepGradientView = new SweepGradientView(MainActivity.this);
         setContentView(R.layout.activity_main);
+
+        //使得布局延伸到状态栏和导航栏区域
+        Window window = getWindow();
+
+//        //实现状态栏图标和文字颜色为浅色
+//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        //实现状态栏图标和文字颜色为暗色
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        //透明状态栏/导航栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                window.setNavigationBarDividerColor(Color.BLUE);
+            }
+        }
+
+//        //实现状态栏图标和文字颜色为暗色
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//
+//        //实现状态栏图标和文字颜色为浅色
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 //        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 //        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
