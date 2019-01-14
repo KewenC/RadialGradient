7150a8cb9bbb381f711884b2798dd510ad6a1f97
chenkw <chenkw@coocent.net>
2019-01-14 17:13:22 +0800

    更新

--- app/src/main/java/com/kewenc/radialgradient/MainActivity.java
+++ app/src/main/java/com/kewenc/radialgradient/MainActivity.java
@@ -25,19 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         //使得布局延伸到状态栏和导航栏区域
         Window window = getWindow();
-
-//        //实现状态栏图标和文字颜色为浅色
-//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        //实现状态栏图标和文字颜色为浅色
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         //实现状态栏图标和文字颜色为暗色
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
+//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         //透明状态栏/导航栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setStatusBarColor(Color.TRANSPARENT);
             window.setNavigationBarColor(Color.TRANSPARENT);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                window.setNavigationBarDividerColor(Color.BLUE);
-            }
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+//                window.setNavigationBarDividerColor(Color.BLUE);
+//            }
         }
 
 //        //实现状态栏图标和文字颜色为暗色
---
+++ app/src/main/java/com/kewenc/radialgradient/RadialGradientView2.java
@@ -0,0 +1,45 @@
+package com.kewenc.radialgradient;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RadialGradientView2 extends View {
+
+    Paint mPaint = null;
+    // 环形渐变渲染
+    Shader mRadialGradient = null;
+
+    public RadialGradientView2(Context context) {
+        super(context);
+        //1.圆心X坐标2.Y坐标3.半径 4.颜色数组 5.相对位置数组，可为null 6.渲染器平铺模式
+        mRadialGradient = new RadialGradient(240, 240, 240, new int[] {
+                Color.YELLOW, Color.GREEN, Color.TRANSPARENT, Color.RED }, null,
+                Shader.TileMode.REPEAT);
+
+        mPaint = new Paint();
+    }
+
+    public RadialGradientView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadialGradientView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // 绘制环形渐变
+        mPaint.setShader(mRadialGradient);
+        // 第一个,第二个参数表示圆心坐标
+        // 第三个参数表示半径
+        canvas.drawCircle(240, 360, 200, mPaint);
+    }
+}
